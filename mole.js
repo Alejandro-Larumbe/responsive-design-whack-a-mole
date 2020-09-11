f10655f50e1d4ef9d776a2a8f06c06af0526db7a
Alejandro Larumbe <larumbemar@gmail.com>
2020-09-12 08:05:43 +1200

    workin on poppin

--- mole.js
+++ mole.js
@@ -1,10 +1,22 @@
-// Write your JS here.
-window.addEventListener('DOMContentLoaded', () => {
-    // setInterval(() => {
-    //     const moleHeads = document.querySelectorAll('.wgs__mole-head');
-    //     for (let moleHead of moleHeads) {
-    //         moleHead.classList.toggle('wgs__mole-head--hidden');
-    //     }
-    // }, 1000);
+let timerPopper;
 
-});
+function getRamdomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max- min + 1)) + min;
+}
+
+function popUpRandomMole() {
+    let moleHeads = document.querySelectorAll(".wsg__mole-head")
+    const moleIndex = getRamdomIntInclusive(1, 7);
+    const moleHead = moleHeads[moleIndex]
+
+    setTimeout(() => {
+
+        hideMole(moleHead)
+    }, 1000);
+}
+
+function hideMole(mole) {
+    mole.classList.add('wgs__mole-head--hidden')
+};
